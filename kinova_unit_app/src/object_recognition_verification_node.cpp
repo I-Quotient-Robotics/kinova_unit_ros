51f3057126962d9757d2848eb1ae6b09cd501dbd
FProgrammerLIU <prog_liu_study@163.com>
2020-06-27 15:34:45 +0800

    fix

--- kinova_unit_app/src/object_recognition_verification_node.cpp
+++
@@ -1,9 +0,0 @@
-#include "kinova_unit_app/object_recognition_verification.h"
-
-int main (int argc, char** argv) {
-  ros::init (argc, argv, "object_detect_node");
-
-  ros::NodeHandle nh, pnh("~");
-  ObjectDetect object_detect(&nh, &pnh);
-  ros::spin();
-}
--- kinova_unit_app/src/object_save_node.cpp
+++
@@ -1,8 +0,0 @@
-#include "kinova_unit_app/object_save.h"
-
-int main (int argc, char** argv) {
-  ros::init (argc, argv, "object_detect_node");
-  ros::NodeHandle nh, pnh("~");
-  ObjectDetect object_detect(&nh, &pnh);
-  ros::spin();
-}
